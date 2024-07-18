becef0972f655a4c4c62379545c2ec252adcbd56
数驱互动\28745 <2874536645@qq.com>
2024-07-18 20:22:55 +0800

    屏幕适配功能添加

--- assets/script/Main.ts
+++ assets/script/Main.ts
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: hanyajun
  * @Date: 2024-06-27 10:43:11
- * @LastEditTime: 2024-06-27 10:43:47
+ * @LastEditTime: 2024-07-18 20:21:23
  */
 
 
@@ -11,19 +11,65 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class NewClass extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
+    protected onLoad(): void {
+        this.initData();
+        cc.view.setResizeCallback(this.canvasChange.bind(this));
+        this.canvasChange();
+    }
 
-    @property
-    text: string = 'hello';
-
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {}
-
-    start() {
+    private initData(): void {
 
     }
 
-    // update (dt) {}
+
+    /**
+     * @description: 点击事件
+     * @return {*}
+     */
+    public onClick(): void {
+        (window as any).openAppStore();
+    }
+
+    protected onDestroy(): void {
+
+    }
+
+    /**
+     * @description: 屏幕适配
+     * @return {*}
+     */
+    private canvasChange(): void {
+        const visibleSize: cc.Size = cc.view.getVisibleSize();
+        const designSize: cc.Size = cc.view.getDesignResolutionSize();
+        if (visibleSize.height / visibleSize.width > designSize.height / designSize.width) {
+            // 竖屏
+            cc.view.setDesignResolutionSize(1080, 1920, cc.ResolutionPolicy.FIXED_WIDTH);
+            // this.vec.active = true;
+            // this.hoz.active = false;
+            this.fingerAnimation(true);
+        } else {
+            // 横屏
+            cc.view.setDesignResolutionSize(1920, 1080, cc.ResolutionPolicy.FIXED_HEIGHT);
+            // this.hoz.active = true;
+            // this.vec.active = false;
+            this.fingerAnimation(false);
+        }
+    }
+
+    private fingerAnimation(type: boolean): void {
+        if (type) {
+            // 竖屏
+            // this.vecAnim.play('vecAnimation');
+            // this.hozAnim.stop();
+        } else {
+            // 横屏
+            // this.vecAnim.stop();
+            // this.hozAnim.play('hozAnimation');
+        }
+        this.schedule(() => {
+            console.log('用户无操作!!!');
+            this.unscheduleAllCallbacks();
+            // this.onClick();
+        }, 22);
+    }
 }
